bf954edc616ccc492dd3d6d1df56eedd986db391
AceS0 <abdulcelilsekerci@gmail.com>
2025-09-26 10:38:48 +0200

    Initial Project Start - 2AE

---
+++ src/main/java/dk/aae/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package dk.aae.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dk/aae/backend/controller/Controller.java
@@ -0,0 +1,7 @@
+package dk.aae.backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+}
